c0d7ae71012a60e4b1189fdf909447f08d3a4f83
Jhonny Tiscareño <jhonnytisca@hotmail.com>
2017-01-09 15:21:14 -0600

    Arreglados meta datos

--- application/language/spanish/inicio/inicio_index_lang.php
+++ application/language/spanish/inicio/inicio_index_lang.php
@@ -3,62 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['language'] = 'es-MX';
 $lang['title_tag'] = 'Renta Antiguo - Inicio';
-$lang['metatag_desc'] = 'En Oficinas Virtuales Amuebladas, nos interesa promover una imagen empresarial para nuestros clientes. Navega por nuestro sitio web y explora nuestros recorridos virtuales 360°.';
-$lang['metatag_keywords'] = 'Oficinas, Virtuales, Amuebladas, Inicio, Recorrido, 360, 3D';
-
-$lang['inicio_slider_1_titulo'] = 'Recorrido 360°';
-$lang['inicio_slider_1_sub'] = 'Explora nuestras sucursales de una forma revolucionaria.';
-$lang['inicio_slider_1_boton'] = 'Velo por ti mismo';
-$lang['inicio_slider_2_titulo'] = 'Nueva imagen';
-$lang['inicio_slider_2_sub'] = 'Navega por nuestro nuevo sitio web.';
-$lang['inicio_slider_3_titulo'] = 'La experiencia nos respalda';
-$lang['inicio_slider_3_sub'] = 'En OVA hacemos crecer tu negocio.';
-
-$lang['inicio_contacto_mobile'] = 'Contactanos';
-
-$lang['inicio_servicios_titulo'] = 'Servicios';
-$lang['inicio_servicios_virtual_titulo'] = 'Oficinas Virtuales';
-$lang['inicio_servicios_virtual_cont'] = 'El servicio ideal para profesionistas y empresas con la necesidad de expandirse a nuevos mercados.';
-$lang['inicio_servicios_amue_titulo'] = 'Oficinas Amuebladas';
-$lang['inicio_servicios_amue_cont'] = 'Ofrecemos espacios totalmente equipados con personal profesional listo para asistirle.';
-$lang['inicio_servicios_juntas_titulo'] = 'Sala de Juntas';
-$lang['inicio_servicios_juntas_cont'] = 'Lleve a cabo sus reuniones y juntas de trabajo en un ambiente profesional.';
-$lang['inicio_servicios_cap_titulo'] = 'Sala de Capacitaciones*';
-$lang['inicio_servicios_cap_cont'] = 'Ofrezca una experiencía única en nuestra Sala de Capacitaciones con las comodidades que su negocio merece. *Solo en sucursal Insurgentes Sur';
-
-$lang['inicio_lema'] = 'En OVA no rentamos oficinas, contribuimos a que su negocio crezca.';
-
-$lang['inicio_beneficios_titulo'] = 'Beneficios';
-$lang['inicio_beneficios_1_titulo'] = 'Varias Sucursales';
-$lang['inicio_beneficios_1_cont'] = 'Como asociado puedes hacer uso de cualquiera de nuestras sucursales.';
-$lang['inicio_beneficios_2_titulo'] = 'Internet Inalámbrico';
-$lang['inicio_beneficios_2_cont'] = 'En todas nuestras sucursales contamos con internet de alta velocidad.';
-$lang['inicio_beneficios_3_titulo'] = 'Asistencia Ejecutiva';
-$lang['inicio_beneficios_3_cont'] = 'Contamos con personal profesional de 9:00 a 18:30 horas.';
-$lang['inicio_beneficios_4_titulo'] = 'Llamadas Ilimitadas';
-$lang['inicio_beneficios_4_cont'] = '¡Puedes hacer llamadas a números fijos, locales y nacionales ilimitadas!';
-$lang['inicio_beneficios_5_titulo'] = 'Servicios Incluidos';
-$lang['inicio_beneficios_5_cont'] = 'Tus planes incluyen el costo de todos los servicios (coffee break, agua, luz, internet, teléfono, limpieza y mantenimiento).';
-$lang['inicio_beneficios_6_titulo'] = 'Sala de Juntas';
-$lang['inicio_beneficios_6_cont'] = 'Proyecta una imagen más profesional haciendo tus reuniones en nuestras salas de juntas.';
-
-$lang['inicio_costos_fiscal_titulo'] = 'Fiscal';
-$lang['inicio_costos_fiscal_basico_titulo'] = 'Fiscal Básico';
-$lang['inicio_costos_fiscal_plus_titulo'] = 'Fiscal Plus';
-$lang['inicio_costos_sub'] = 'Los costos pueden variar por sucursal';
-$lang['inicio_costos_tiempo_abbr'] = 'mes';
-$lang['inicio_costos_tiempo'] = 'mensual';
-$lang['inicio_costos_boton'] = 'Ver Sucursales';
-$lang['inicio_costos_amue_titulo'] = '¿Qué beneficios me ofrece?';
-$lang['inicio_costos_amue_1'] = ' Oficinas bien equipadas.';
-$lang['inicio_costos_amue_2'] = ' Excelentes ubicaciones.';
-$lang['inicio_costos_amue_3'] = ' Sala de capacitaciones.';
-$lang['inicio_costos_amue_4'] = ' Salas de juntas.';
-$lang['inicio_costos_amue_5'] = ' Precios preferenciales.';
-$lang['inicio_costos_amue_6'] = ' Internet inalámbrico.';
-$lang['inicio_costos_amue_7'] = ' Recepción de mensajería.';
-$lang['inicio_costos_amue_8'] = ' Y más...';
-$lang['inicio_costos_amue_precio'] = 'Costos de acuerdo al espacio y la ubicación';
-$lang['inicio_costos_fiscal_plus'] = 'Conoce nuestros planes Fiscales Plus';
+$lang['metatag_desc'] = 'Alquila un auto clásico para tus eventos sociales a un precio especial. Dodge Club Coupe 1946.';
+$lang['metatag_keywords'] = 'Clásico, Auto, Recorrido, Evento, Social, Antiguo, Renta';
 
 ?>
--- application/language/spanish/menus/menu_uno_lang.php
+++ application/language/spanish/menus/menu_uno_lang.php
@@ -1,13 +1,6 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$lang['menu_llamanos'] = 'Llámanos: ' . TEL_VENTAS;
 $lang['menu_inicio'] = 'Inicio';
 $lang['menu_contacto'] = 'Contacto';
 $lang['menu_servicio'] = 'Servicio';
-
-$lang['menu_oficina'] = 'Oficinas';
-$lang['menu_recepcion'] = 'Recepción';
-$lang['menu_sala'] = 'Sala de Juntas';
-$lang['menu_capacitacion'] = 'Sala de Capacitaciones';
-$lang['menu_proximo'] = 'Proximamente';
