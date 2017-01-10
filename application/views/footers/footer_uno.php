7279307b196b077f9a1e25653af64441b4b9f2c9
Jhonny Tiscareño <jhonnytisca@hotmail.com>
2017-01-09 22:24:29 -0600

    Footer arreglado

--- application/views/footers/footer_uno.php
+++ application/views/footers/footer_uno.php
@@ -16,7 +16,7 @@
                     Copyright © 2017 Todos los derechos reservados por <strong><?php echo ICOGNITIS_SACV;?></strong><br>
                     <div class="copyright-links"><a href="<?php echo ICOGNITIS_URL;?>">Página del desarrollador</a></div>
                 </div>
-
+                <br class="visible-xs visible-sm"/>
                 <div class="col_half col_last tright">
                     <div class="fright clearfix">
                         <a href="<?php echo FACEBOOK;?>" target="_blank" class="social-icon si-small si-borderless si-facebook">
@@ -32,7 +32,8 @@
 
                     <div class="clear"></div>
 
-                    <i class="icon-email3" style="margin-right: 5px;"></i>&nbsp;<?php echo MAIL_VENTAS;?> <span class="middot">·</span>
+                    <i class="icon-email3" style="margin-right: 5px;"></i>&nbsp;<?php echo MAIL_VENTAS;?> <span class="middot hidden-xs hidden-sm">·</span>
+                    <br class="visible-xs visible-sm"/>
                     <i class="icon-call" style="margin-right: 5px;"></i>&nbsp;<?php echo TEL_VENTAS;?>
                 </div>
 
